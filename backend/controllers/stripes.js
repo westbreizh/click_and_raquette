1df2c8a165d771aaa8fe1710b9376a9eed10108d
aurélien <herbreteauaurelien@hotmail.fr>
2023-06-14 14:16:35 +0200

    refonte pour deploiement avec netlify et heroku

--- backend/controllers/stripes.js
+++ backend/controllers/stripes.js
@@ -1,9 +1,35 @@
 const stripe = require('stripe')('sk_test_51NGdYqI8HrVwrRfPKAmQ17TgZh2yWZtGjNNqhHyMXhebWNh03YR5zgGhibzt5oHJM1eRD5UrwRAvhZPNhs48fC9L00UjaCIuJq');
-const YOUR_DOMAIN = 'http://localhost:3001';
+const YOUR_DOMAIN = 'http://localhost:3000';
 
 
 
-exports.createCheckOutSession = async (req, res) => {
+exports.createCheckOutSession2 = async (req, res) => {
+
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1NIf38I8HrVwrRfPhB9jwg4q',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    automatic_tax: {enabled: true},
+  });
+
+  res.redirect(303, session.url);
+};
+  
+  
+
+
+
+
+  
+  exports.createCheckOutSession = async (req, res) => {
 
     try {
 
@@ -27,13 +53,12 @@ exports.createCheckOutSession = async (req, res) => {
       });
       console.log("je suis dans stripe avant redirection");
       res.redirect(303, session.url);
-      //console.log(res)
+
+
       console.log(session.url)
+      
     } catch (error) {
       console.error('Une erreur est survenue lors de la création de la session de paiement', error);
       res.status(500).json({ error: 'Erreur lors de la création de la session de paiement' });
     }
   };
-  
-  
-  
--- frontend/src/stripe/CheckoutForm.jsx
+++ frontend/src/stripe/CheckoutForm.jsx
@@ -1,7 +1,10 @@
 import { useStripe } from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
+
+  console.log("erby");
   const stripe = useStripe();
+
   const handleCheckout = async () => {
     // Créez une session de paiement avec les détails appropriés
     const response = await fetch('http://localhost:3001/api/stripe/create-checkout-session', {
@@ -9,15 +12,19 @@ const CheckoutForm = () => {
       headers: {
         'Content-Type': 'application/json',
       },
+      
     });
+
     const session = await response.json();
     console.log(session);
     // Redirigez l'utilisateur vers la page de paiement Stripe
     const result = await stripe.redirectToCheckout({
+      
       sessionId: session.id,
     });
 
     if (result.error) {
+      console.log("stripeerreur");
       // Gérez les erreurs de redirection
       console.error(result.error.message);
     }
